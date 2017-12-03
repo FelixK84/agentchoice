b7f56a8a84bfff228f59fde34cf29a74b516bcfc
Sven <sven@mutzl.org>
2017-12-03 13:09:29 +0000

    Promise action

--- bot.js
+++ bot.js
@@ -4,40 +4,48 @@ var mysql_modul = require(path.join(__dirname, 'module', 'mysql_modul'));
 
 var da = [];
 
+
+
 var job = new CronJob('*/4 * * * * *', () => {
-  mysql_modul.getrate_btceth( (err, results) => {
-    da[0] = {'btceth' : results[0].rate};
-    //console.log(results[0].rate);
-  });
-  mysql_modul.getrate_ethzec( (err, results) => {
-    da[1] = {'ethzec' : results[0].rate};
-    //console.log(results[0].rate.toFixed(8));
-  });
-  mysql_modul.getrate_btczec( (err, results) => {
-    da[2] = {'btczec' : results[0].rate};
-    //console.log(results[0].rate);
-  });
-  if(da.length !== 0){
-    console.log('btceth: ' + da[0].btceth);
-    console.log('ethzec: ' + da[1].ethzec);
-    console.log('btczec: ' + da[2].btczec);
-  }
-  if(da.length !== 0){
-    console.log(calcho(da));
-    console.log("Abzgl. der Transaktionskosten: " + (calcho(da)-(0.0025*3)));
-    var fac = calcho(da)-(0.0025*3);
-    if(fac > 1.02){
-      mysql_modul.insert_factor(fac, da, (err, suc) => {
-        if(err){
-          console.error(err);
-        } else {
-          console.log("Eine Möglichkeit steht zur Verfügung");
-        }
-      });
-    }
-  }
+    Promise.all([
+            mysql_modul.getrate('btc_eth'),
+            mysql_modul.getrate('eth_zec'),
+            mysql_modul.getrate('btc_zec'),
+        ])
+        .then((rates) => {
+            da[0] = {
+                'btceth': rates[0]
+            };
+            da[1] = {
+                'ethzec': rates[1]
+            };
+            da[2] = {
+                'btczec': rates[2]
+            };
+        })
+        .then(() => {
+            if (da.length !== 0) {
+                console.log('btceth: ' + da[0].btceth);
+                console.log('ethzec: ' + da[1].ethzec);
+                console.log('btczec: ' + da[2].btczec);
+            }
+            if (da.length !== 0) {
+                console.log(calcho(da));
+                console.log("Abzgl. der Transaktionskosten: " + (calcho(da) - (0.0025 * 3)));
+                var fac = calcho(da) - (0.0025 * 3);
+                if (fac > 1.02) {
+                    mysql_modul.insert_factor(fac, da, (err, suc) => {
+                        if (err) {
+                            console.error(err);
+                        } else {
+                            console.log("Eine Möglichkeit steht zur Verfügung");
+                        }
+                    });
+                }
+            }
+        });
 }, null, true);
 
-function calcho(da){
-  return ((da[0].btceth) * (da[1].ethzec) * (1/da[2].btczec));
+function calcho(da) {
+    return ((da[0].btceth) * (da[1].ethzec) * (1 / da[2].btczec));
 }
--- module/mysql_modul.js
+++ module/mysql_modul.js
@@ -4,130 +4,150 @@ var dbc = require(path.join(__dirname, '..', 'config', 'dbc'));
 
 var Mysql_modul = {
 
-  insert_factor: (factor, ratearray, callback) => {
+    insert_factor: (factor, ratearray, callback) => {
 
-    console.log(ratearray);
+        console.log(ratearray);
 
-    /*  the datum var has the format dd.MM.yyyy when it comes.
-        for the query i need either integer or the timestamp format!  */
-    dbc.getconnection((err, con) => {
-        con.connect();
+        /*  the datum var has the format dd.MM.yyyy when it comes.
+            for the query i need either integer or the timestamp format!  */
+        dbc.getconnection((err, con) => {
+            con.connect();
 
-        var queryString = "INSERT INTO choices SET ?";
+            var queryString = "INSERT INTO choices SET ?";
 
-        var values = {
-          factor: factor,
-          btceth: ratearray[0].btceth,
-          ethzec: ratearray[1].ethzec,
-          btczec: ratearray[2].btczec,
-          status: 0
-        };
+            var values = {
+                factor: factor,
+                btceth: ratearray[0].btceth,
+                ethzec: ratearray[1].ethzec,
+                btczec: ratearray[2].btczec,
+                status: 0
+            };
 
-        con.query(queryString, values, (err, results, fields) => {
-          con.end();
-          if(err){
-            callback(err);
-          } else {
-            callback(null, true)
-          }
+            con.query(queryString, values, (err, results, fields) => {
+                con.end();
+                if (err) {
+                    callback(err);
+                } else {
+                    callback(null, true)
+                }
+            });
         });
-    });
-  },
+    },
 
-  getrate_btceth: (callback) => {
-    dbc.getconnection((err, con) => {
-        con.connect();
+    getrate: (pair) => {
+        return new Promise((resolve, reject) => {
+            dbc.getconnection((err, con) => {
+                con.connect();
 
-        var queryString = "SELECT * FROM btc_eth where id = (select max(id) from btc_eth)";
+                var queryString = "SELECT rate FROM ? ORDER BY `id` DESC LIMIT 1";
 
-        /*var values = {
-
-        };*/
-
-        con.query(queryString, /*values,*/ (err, results, fields) => {
-          con.end();
-          if(err){
-            callback(err);
-          } else {
-            callback(null, results)
-          }
+                con.query(queryString, [pair, pair], (err, results, fields) => {
+                    con.end();
+                    if (err) {
+                        console.error(err);
+                        reject(Error(err.message));
+                    } else {
+                        resolve(results.rate);
+                    }
+                });
+            });
         });
-    });
-  },
+    },
 
-  getrate_btczec: (callback) => {
-    dbc.getconnection((err, con) => {
-        con.connect();
+    getrate_btceth: (callback) => {
+        dbc.getconnection((err, con) => {
+            con.connect();
 
-        var queryString = "SELECT * FROM btc_zec where id = (select max(id) from btc_zec)";
+            var queryString = "SELECT * FROM btc_eth where id = (select max(id) from btc_eth)";
 
-        /*var values = {
+            /*var values = {
 
-        };*/
+            };*/
 
-        con.query(queryString, /*values,*/ (err, results, fields) => {
-          con.end();
-          if(err){
-            callback(err);
-          } else {
-            callback(null, results)
-          }
+            con.query(queryString, /*values,*/ (err, results, fields) => {
+                con.end();
+                if (err) {
+                    callback(err);
+                } else {
+                    callback(null, results)
+                }
+            });
         });
-    });
-  },
+    },
 
-  getrate_ethzec: (callback) => {
-    dbc.getconnection((err, con) => {
-        con.connect();
+    getrate_btczec: (callback) => {
+        dbc.getconnection((err, con) => {
+            con.connect();
 
-        var queryString = "SELECT * FROM eth_zec where id = (select max(id) from eth_zec)";
+            var queryString = "SELECT * FROM btc_zec where id = (select max(id) from btc_zec)";
 
-        /*var values = {
+            /*var values = {
 
-        };*/
+            };*/
 
-        con.query(queryString, /*values,*/ (err, results, fields) => {
-          con.end();
-          if(err){
-            callback(err);
-          } else {
-            callback(null, results)
-          }
+            con.query(queryString, /*values,*/ (err, results, fields) => {
+                con.end();
+                if (err) {
+                    callback(err);
+                } else {
+                    callback(null, results)
+                }
+            });
         });
-    });
-  },
+    },
 
-  update_time: (id, date, from, to, callback) => {
+    getrate_ethzec: (callback) => {
+        dbc.getconnection((err, con) => {
+            con.connect();
 
-    dbc.getconnection((err, con) => {
-      con.connect();
+            var queryString = "SELECT * FROM eth_zec where id = (select max(id) from eth_zec)";
 
-      var queryString = "UPDATE times SET arbeitsbeginn = ?, arbeitsende = ? WHERE id = ?";
+            /*var values = {
 
-      //console.log(Time_modul.makedateobj(date, from));
+            };*/
 
-      var values = [
-        Time_modul.makedateobj(date, from),
-        Time_modul.makedateobj(date, to),
-        id
-      ];
+            con.query(queryString, /*values,*/ (err, results, fields) => {
+                con.end();
+                if (err) {
+                    callback(err);
+                } else {
+                    callback(null, results)
+                }
+            });
+        });
+    },
 
-      //console.log(values[0]);
-      //console.log(values[1]);
-      //console.log(values.id);
-      //console.log(values.arbeitsbeginn);
-      //console.log(values.arbeitsende);
+    update_time: (id, date, from, to, callback) => {
 
-      con.query(queryString, values, (err, results, fields) => {
-        con.end();
-        if(err){
-          callback(err);
-        } else {
-          callback(null, true)
-        }
-      });
-    });
-  }
+        dbc.getconnection((err, con) => {
+            con.connect();
+
+            var queryString = "UPDATE times SET arbeitsbeginn = ?, arbeitsende = ? WHERE id = ?";
+
+            //console.log(Time_modul.makedateobj(date, from));
+
+            var values = [
+                Time_modul.makedateobj(date, from),
+                Time_modul.makedateobj(date, to),
+                id
+            ];
+
+            //console.log(values[0]);
+            //console.log(values[1]);
+            //console.log(values.id);
+            //console.log(values.arbeitsbeginn);
+            //console.log(values.arbeitsende);
+
+            con.query(queryString, values, (err, results, fields) => {
+                con.end();
+                if (err) {
+                    callback(err);
+                } else {
+                    callback(null, true)
+                }
+            });
+        });
+    }
 }
 
 module.exports = Mysql_modul;
